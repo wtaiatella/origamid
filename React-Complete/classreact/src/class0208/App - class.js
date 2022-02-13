3b7527d10610cd8ea845439367bdf5ca08b811b6
Wagner Taiatella <wtaiatella@gmail.com>
2022-02-13 12:47:44 -0400

    update class208

---
+++ React-Complete/classreact/src/class0208/App - class.js
@@ -0,0 +1,30 @@
+const App = () => {
+  /*   const filmes = ['Before Sunrise', 'Before sunset', 'Before Midnight']
+
+  return (
+    <ul>
+      {filmes.map((filme) => (
+      <li key={filme}>{filme}</li>))}
+    </ul>
+  )*/
+
+  const livros = [
+    { nome: 'A Game of Thrones', ano: 1996 },
+    { nome: 'A Clash of Kings', ano: 1998 },
+    { nome: 'A Storm of Swords', ano: 2000 },
+  ]
+
+  return (
+    <ul>
+      {livros
+        .filter(({ ano }) => ano >= 1998)
+        .map(({ nome, ano }) => (
+          <li key={nome}>
+            {nome}, {ano}
+          </li>
+        ))}
+    </ul>
+  )
+}
+
+export default App
