c364dff05f503bacc2a3f50e290fb36106f9cb01
Wagner Taiatella <51032625+wtaiatella@users.noreply.github.com>
2022-03-20 18:21:27 -0300

    React course - class0304 - useMemo example

--- React-Complete/classreact/src/App.js
+++ React-Complete/classreact/src/App.js
@@ -1,47 +1,29 @@
 import React from 'react'
-//import Product from './class0302 - exercise/Product'
+
+/*
+UseMemo is used to prevent that a slow function runs at every render and if it is not necessary
+the function just runs once or when the dependency is updated
+
+*/
+
+//This is a example of slow function
+function slowFunction() {
+  let c
+  for (let i = 0; i < 100000000; i++) {
+    c = i + i / 10
+  }
+  return c
+}
 
 const App = () => {
-  const [comments, setComments] = React.useState([])
-  const [input, setInput] = React.useState('')
-  const inputElement = React.useRef()
+  const [count, setCount] = React.useState(0)
+  //register the time reference
+  const t1 = performance.now()
+  const value = React.useMemo(() => slowFunction(), [])
+  //log the time diference, about 100ms
+  console.log(performance.now() - t1)
 
-  const timeoutRef = React.useRef()
-
-  function handleClick() {
-    setComments([...comments, input])
-    setInput('')
-    inputElement.current.focus()
-
-    //the reference is created to be possible to reach the timeout created in the event before.
-    //this ways de current timeout is clear and a new one started
-    //the useRef here prevents to the timeout don't reset when a new comment is added,
-    clearTimeout(timeoutRef.current)
-    timeoutRef.current = setTimeout(() => {
-      setComments([])
-    }, 10000)
-  }
-
-  return (
-    <div>
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment}>{comment}</li>
-        ))}
-      </ul>
-      <input
-        type="text"
-        //Creact reference for this element
-        //At this example the useRef is used to create a reference for the input element
-        //This way is possible to focus it without using getElement from the DOM.
-        ref={inputElement}
-        value={input}
-        onChange={({ target }) => setInput(target.value)}
-      />
-      <br />
-      <button onClick={handleClick}>Send</button>
-    </div>
-  )
+  return <button onClick={() => setCount(count + 1)}>{count}</button>
 }
 
 export default App
